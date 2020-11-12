029d154a4df43605986d465b2ae7abe3a2e9f79c
Randy <ranmolina@gmail.com>
2020-11-12 01:36:45 -0500

    postcss update

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,23 @@
 module.exports = {
-  plugins: ["tailwindcss", "postcss-preset-env"],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+
+    ...(process.env.NODE_ENV === "production"
+      ? {
+          "@fullhuman/postcss-purgecss": {
+            content: [
+              "./components/**/*.*",
+              "./pages/**/*.*",
+              "./contexts/**/*.*",
+              "./gql/**/*.*",
+              "./utils/**/*.*",
+              "./public/**/*.*",
+            ],
+            defaultExtractor: (content) =>
+              content.match(/[\w-/.:]+(?<!:)/g) || [],
+          },
+        }
+      : {}),
+  },
 };
